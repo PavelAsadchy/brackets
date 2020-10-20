ec5849939613acdc101bf3fa54c0f449c4795a57
pavel asadchy <asadchy.pavel@gmail.com>
2020-10-20 22:53:31 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let brackets = {};
+
+	bracketsConfig.forEach(pair => {
+		brackets[pair[0]] = pair[1];
+	});
+
+	let stack = [];
+	let copiesStack = [];
+
+	for (let i = 0; i < str.length; i++) {
+		if (Object.keys(brackets).includes(str[i]) && Object.values(brackets).includes(str[i]) && stack.includes(str[i])) {
+			stack.pop();
+		} else if (brackets.hasOwnProperty(str[i])) {
+			stack.push(str[i]);
+		} else if (brackets[stack[stack.length - 1]] === str[i]) {
+			stack.pop();
+		} else if (!stack.length) return false;
+	};
+
+	return stack.length > 0 ? false : true;
 }
